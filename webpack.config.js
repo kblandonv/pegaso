7b4f1e2c6fbcd7213bfff60f52d20003240a7b4a
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-01-23 16:09:24 +0000

    test: webpack deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     output: {
-        filename: 'bundle.js',
+        filename: 'index.js',
         path: path.resolve(__dirname, 'src'),
     },
 };
