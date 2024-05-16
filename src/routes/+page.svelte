4dea778f5783f5fbd0b1d72df0a190762c0eaee9
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-05-16 18:43:18 +0000

    delete layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,28 @@
 <script>
 	const { data } = $props();
-	import Buscador from "$components/Buscador";
-	import Horario from "$components/Horario";
-	import { GraficoCupos, GraficoDistribucion, GraficoDocente }  from "$components/Horario/graficos";
 
-	import { storeAsignaturas } from "$lib/stores/asignaturas.svelte.js";
+	import { setContext } from 'svelte';
+	import Header from '$components/UI/Header.svelte';
+	import Footer from '$components/UI/Footer.svelte';
+	import Blob from '$components/UI/Blob.svelte';
+	import Toast from '$components/UI/Toast.svelte';
+	import '$src/styles/global.scss';
+
+	import Buscador from '$components/Buscador';
+	import Horario from '$components/Horario';
+	import { GraficoCupos, GraficoDistribucion, GraficoDocente } from '$components/Horario/graficos';
+
+	import { storeAsignaturas } from '$lib/stores/asignaturas.svelte.js';
 	storeAsignaturas.data = data.asignaturas;
 
+	let toastInstance;
+	function addToast(mensaje) {
+		toastInstance.addToast(mensaje);
+	}
+
+	setContext('toast', {
+		addToast
+	});
 </script>
 
 <svelte:head>
@@ -14,20 +30,27 @@
 	<meta name="description" content="Sia creado por imlargo" />
 </svelte:head>
 
-<main>
+<main class="container">
+	<Header />
 
-	<!-- Buscador de cursos -->
-	<Buscador horarioLocal={data.horarioLocal} />
+	<section>
+		<!-- Buscador de cursos -->
+		<Buscador horarioLocal={data.horarioLocal} />
 
-	<hr class="hr-pink" />
+		<hr class="hr-pink" />
 
-	<GraficoCupos />
-	<GraficoDistribucion />
-	<GraficoDocente />
+		<GraficoCupos />
+		<GraficoDistribucion />
+		<GraficoDocente />
 
-	<!-- Horario -->
-	<Horario logDescargaEvent={data.logDescargaEvent} />
+		<!-- Horario -->
+		<Horario logDescargaEvent={data.logDescargaEvent} />
+	</section>
 
+	<Footer />
+	<div id="toast-container" class="toast-container position-fixed bottom-0 end-0 p-3"></div>
+
+	<Toast bind:this={toastInstance} />
 </main>
 
 <style lang="scss">
