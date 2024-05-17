a1b07b2c7a55c7d2f9fdc6902dbd96661ff80f67
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-05-17 20:44:43 +0000

    fix toast on update cupos

--- src/lib/stores/asignaturas.svelte.js
+++ src/lib/stores/asignaturas.svelte.js
@@ -12,12 +12,19 @@ class StoreAsignaturas {
     async initMongo() {
         const collAsignaturas = await initMongo();
 
+        let count = 0;
         for await (const change of collAsignaturas.watch()) {
+            count +=1;
+
             const { documentKey, fullDocument } = change;
             const facultad = documentKey._id;
 			delete fullDocument._id;
 			this.data[facultad] = fullDocument;
-            this.updated = true;
+            
+            if (count === 5) {
+                this.updated = true;
+                count = 0;
+            }
         }
 
     }
