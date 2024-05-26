db3a094bf68513b96b2dc992321fb6e3613a47e1
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-05-26 17:57:55 +0000

    improve horario borders

--- src/components/Horario/CeldaHorario.svelte
+++ src/components/Horario/CeldaHorario.svelte
@@ -2,7 +2,7 @@
     const { id, color, materia } = $props();
 </script>
 
-<td {id} class={`text-xs my-border px-4 py-2 ${color}`}>{materia}</td>
+<td {id} class={`text-xs px-4 py-2 ${color ? color : "my-border"}`}>{materia}</td>
 
 <style>
 
